0bf3dbe262efd66475fe4939b39325f2b4cdd6cb
Sagar Baisoya <88628597+sagar1128@users.noreply.github.com>
2021-09-24 22:33:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+
+    $('#menu').click(function(){
+        $(this).toggleClass('fa-times');
+        $('.navbar').toggleClass('nav-toggle');
+    });
+
+    $(windows).on('scroll load',function(){
+        $(this).removeClass('fa-times');
+        $('.navbar').removeClass('nav-toggle');
+
+        if($(windows).scrollTop()>0){
+            $('.scroll-top').show();
+        }
+        else{
+            $('.scroll-top').hide(); 
+        }
+
+        // scroll spy
+        $('section').each(function(){
+            let height=$(this).height();
+            let offset=$(this).offset().top - 200;
+            let top=$(window).scrollTop();
+            let id=$(this).attr('id');
+
+            if(top>offset && top<offset+height){
+                $('.navbar ul li a').removeClass('active');
+                $('.navbar').find(`[href="#${id}"]`).addClass('active');
+            }
+        });
+    });
+
+    // smooth scrolling
+    $('a[href="#]').on('click',function(e){
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop : $($(this).attr('href')).offset.top,
+        },
+        500,
+        'linear'
+        )
+    })
+});
